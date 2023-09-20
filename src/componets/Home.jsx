b0dd7333091a57a5e626928ebc202c9716a0d9ee
sideffect263 <90728515+sideffect263@users.noreply.github.com>
2023-09-21 01:47:48 +0300

    redodi

---
+++ src/componets/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+import ReactPlayer from "react-player"
+
+
+function Home() {
+    return (
+        <div className="mainHome">
+            <div className="firstItem">
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/componets/NavbarComp.jsx
@@ -0,0 +1,47 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './NavbarComp.css';
+import homeLogo from '../assets/infa/the-box.jpeg'
+
+
+function NavbarComp() {
+    return (
+      <Navbar  sticky='top'  className="bg-body-tertiary">
+        <Container className='conNavbar'>
+            <div className='logoBox'>
+            <img src={homeLogo} alt='logo' className='logo'/>
+          <Navbar.Brand className='brandText' href="#home">fitness&studio</Navbar.Brand>
+          </div>
+          <div className='linkBox'>
+          <Navbar.Toggle className='' aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className='' id="basic-navbar-nav">
+            <Nav className="linkText">
+                <div className='signNow'>
+              <Nav.Link href="#home">בית</Nav.Link>
+              </div>
+
+              <div className='dropdownCon'>
+              <NavDropdown title="תפריט" id="basic-nav-dropdown" className='navDropText'>
+                <NavDropdown.Item href="#action/3.1">הסטודיו</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">סוגי אימונים</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">לוח אימונים</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">טיפולים פיזיוטרפיים</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  צור קשר
+                </NavDropdown.Item>
+              </NavDropdown>
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+          </div>
+        </Container>
+      </Navbar>
+    );
+  }
+  
+  export default NavbarComp;
+  
